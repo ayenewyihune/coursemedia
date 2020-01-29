84975a968b4bf870523038c6b379da40d11898bd
Ayenew Yihune <profoundeng@gmail.com>
2020-01-29 12:04:04 +0300

    First Commit

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,34 +1,173 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('css')
-<link href="{{asset('css/course_sidebar.css') }}" rel="stylesheet">
-@endsection
+<head>
+    <meta charset="utf-8">
+    <meta name="description"
+        content="This is the official webiste of the Ethiopian Association of Civil Engineers (EACE).">
+    <meta name="keywords" content="EACE, Ethiopian, Civil, Engineers">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    {{-- For large screens --}}
-    <div>
-        <h2 class="mt-4 mb-3 display-4">Account Settings</h2>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        @yield('breadcrumb')
-        <div class="row">
-            <!-- Sidebar Column -->
-            <div class="col-lg-3 mb-4">
-                <div class="list-group">
-                    <a href="/dashboard" class="list-group-item">Account Details</a>
-                    <a href="/dashboard/courses" class="list-group-item">Courses</a>
+    <title>{{ config('app.name', 'Ethiopian Association of Civil Engineers (EACE)') }}</title>
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="{{ asset('images/eace_logo.jpg') }}" type="image/x-icon">
+    {{-- Style sheet --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="//l.allcdn.org/fa/v5.3.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    @yield('extra-css')
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="/member-requests" class="brand-link">
+                <img src="{{ asset('images/eace_logo.jpg') }}" alt="EACE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">EACE</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="info">
+                        <a href="#" class="d-block">Admin Dashboard</a>
+                    </div>
                 </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>
+                                    Membership
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/member-requests" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Membership Requests</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/approved-requests" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Approved Requests</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/members" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Members List</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/members/create" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Create Member</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-newspaper-o"></i>
+                                <p>
+                                    Events
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/admin/news" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>News</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/admin/events" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Events</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="/admin/announcements" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Announcements</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-header">MISCELLANEOUS</li>
+                        <li class="nav-item">
+                            <a href="https://adminlte.io/docs/3.0" class="nav-link">
+                                <i class="nav-icon fas fa-file"></i>
+                                <p>Documentation</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
             </div>
-            <!-- Content Column -->
-            <div class="col-lg-9 mb-4">
-                @yield('main')
-            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            @include('inc.messages')
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        @yield('header')
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    @yield('main')
+                </div>
+            </section>
         </div>
-    </div>
-</div>
 
-@endsection
+        <!-- REQUIRED SCRIPTS -->
 
-@section('js')
-<script src="{{ asset('js/course_sidebar.js') }}"></script>
-@endsection
+        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace('main_body');
+
+        </script>
+        @yield('js')
+</body>
+
+</html>
